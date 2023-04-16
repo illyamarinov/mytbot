0c50d304b00b71a22ff3c36067386e70b13559f1
Illya Marinov <ilya.marinov.94@gmail.com>
2023-04-16 20:14:55 +0300

    initialization, add .gitignore

---
+++ index.js
@@ -0,0 +1,9 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const token = '6049087139:AAE0nWkEE2MaRxm4nnYjyiN0Wj2RjrUxOs4';
+const bot = new TelegramBot(token, {polling: true});
+
+bot.onText(/\/start/, (msg, match) => {
+  const chatId = msg.chat.id;
+  bot.sendMessage(chatId, 'Привет, я твой новый бот!');
+});
